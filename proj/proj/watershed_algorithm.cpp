1ea640e59e80ba9d821611362139041b8ee7ef1b
MihailBeloshapkin <misha.beloshapkin@yandex.ru>
2021-03-25 01:50:52 +0300

    some changes

--- proj/proj/watershed_algorithm.cpp
+++ proj/proj/watershed_algorithm.cpp
@@ -16,6 +16,33 @@ namespace MyProject
     void watershed::watershed_alg(Mat frame)
     {
         Mat subFrame = frame(cv::Range(frame.rows / 2, frame.rows), cv::Range(0, frame.cols));
+        Mat gray;
+        Mat bw;
+
+        cvtColor(subFrame, bw, COLOR_BGR2GRAY);
+        threshold(bw, bw, 40, 255, THRESH_BINARY | THRESH_OTSU);
+        Mat dist;
+
+        distanceTransform(bw, dist, DIST_L2, 3);
+
+        normalize(dist, dist, 0, 1.0, NORM_MINMAX);
+        threshold(dist, dist, 0.5, 1.0, THRESH_BINARY);
+
+        Mat kernel1 = Mat::ones(3, 3, CV_8U);
+        dilate(dist, dist, kernel1);
+
+        Mat dist_8u;
+        dist.convertTo(dist_8u, CV_8U);
+
+        vector<vector<Point>> contours;
+        findContours(dist_8u, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+        Mat markers = Mat::zeros(dist.size(), CV_32S);
+
+        for (size_t i = 0; i < contours.size(); i++)
+        {
+            drawContours(markers, contours, static_cast<int>(i), Scalar(static_cast<int>(i) + 1), -1);
+        }
     }
 
     /*
